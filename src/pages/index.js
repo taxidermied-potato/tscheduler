2831055f0af299af7d98826df2604d73e0da61bd
Al Yin <altyin@gmail.com>
2020-02-14 11:39:29 -0500

    Updates

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../Style.css'
-import App from '../App'
-
-export default () => <App />
-
--- src/utils/typography.js
+++
@@ -1,8 +0,0 @@
-import Typography from "typography"
-
-const typography = new Typography({
-  baseFontSize: "14px",
-  baseLineHeight: 1.666,
-})
-
-export default typography
